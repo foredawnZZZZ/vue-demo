3438722bbdf7c84602d0458ab944aa6a9de421b6
foredawnZZZZ <18602151918@163.com>
2020-09-11 14:27:15 +0800

    09-11 first commit vuex~~

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,42 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    city: "shanghai",
+    list: [
+      {
+        id: 1,
+        title: 'Fore'
+      },
+      {
+        id: 2,
+        title: 'Dawn'
+      },
+      {
+        id: 3,
+        title: 'Lin'
+      }
+    ]
+  },
+  getters: {
+    filterList: state => {
+      return state.list.map(item => item.title);
+    }
+  },
+  mutations: {
+    changeCity(state) {
+      state.city = 'beijing'
+    },
+    changeList(state,val) {
+      setTimeout(() => {
+        state.list.push(val);
+      },2000)
+    }
+  },
+  actions: {
+    changeList({commit},val) {
+      commit('changeList',val)
+    }
+  },
   modules: {}
 });
--- src/views/com/Home.vue
+++ src/views/com/Home.vue
@@ -2,16 +2,42 @@
   <div>
     <el-button type="primary" @click="handleToPath('/backToOne')">去页面1</el-button>
     <el-button type="primary" @click="handleToPath('/backToTwo')">去页面2</el-button>
+    <br>
+    <br>
+    <br>
+    <br>
+    <el-button type="primary" @click="handleCommit">{{cityVal || 'test store'}}</el-button>
+    <ul>
+      <li v-for="(item,index) in filterList" :key="index">{{item}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: "home",
+  data() {
+    return {
+      cityVal: ""
+    }
+  },
   methods: {
     handleToPath(path) {
       this.$router.push(path)
-    }
+    },
+    handleCommit() {
+      this.$store.commit('changeCity');
+      this.$store.dispatch('changeList',{
+        title: 'test',
+        id: 4
+      });
+    },
+  },
+  computed : {
+    ...mapGetters([
+      'filterList'
+    ])
   }
 }
 </script>
